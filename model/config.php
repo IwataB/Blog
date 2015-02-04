ea2919db8162c5bb546fabf7ecadaac0e7b5efbe
Bryant Iwata <pbomber07@gmail.com>
2015-02-04 08:45:41 -0800

    End of 2/4/15

--- model/config.php
+++ model/config.php
@@ -1,2 +1,7 @@
 <?php
     $path = "/IwataB-Blog/";
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<?php
+    require_once(__DIR__ ."/../model/config.php");
+?>
+
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
     <div>
         <label for="title">Title: </label>
         <input type="text" name="title" />
@@ -8,5 +12,8 @@
     <div>
         <label for="post">Post: </label>
         <textarea name="post"></textarea>
+    </div>
+    <div>
+        <button type="submit">Submit</button> 
     </div>
 </form>
