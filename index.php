d980f605e1837050b5b4f0ee97828f94bc31946c
Bryant Iwata <pbomber07@gmail.com>
2015-02-26 09:18:16 -0800

    End of 2/26/15

--- index.php
+++ index.php
@@ -6,5 +6,5 @@
             }
             require_once (__DIR__."/controller/create-db.php");
             require_once (__DIR__."/view/footer.php");
-            require_once (__DIR__."/controller/read-post.php")
+            require_once (__DIR__."/controller/read-post.php");
         ?>
--- view/header.php
+++ view/header.php
@@ -1,4 +1,9 @@
 <html>
+    <div id="menu">
+<ul><li><a href="login-user.php">Login</a></li>
+<li><a href="posts.php">Blog Post Form</a></li>
+<li><a href="register.php">Register</a></li>
+</div>
     <head>
         <meta charset="UTF-8">
         <title></title>
